252ee26251b8c261145c7fdfee51b9a05b5a7678
oleg <burtaev.oleg@yandex.ru>
2021-05-23 02:47:14 +0300

    Add: landlord add rental page

--- src/pages/Landlord/LandlordAddRentalsPage.js
+++ src/pages/Landlord/LandlordAddRentalsPage.js
@@ -1,119 +1,118 @@
 import React from 'react'
+import Button from '@/components/Button'
 import Card from '@/components/Card/Card'
 import Container from '@/components/Container'
+import Input from '@/components/Input'
 
 function LandlordAddRentalsPage() {
   return (
-    <div className='flex flex-col items-center w-full h-full bg-gray-100 min-h-screen'>
-      <Container>
-        <h1 className='text-4xl text-gray-800 font-bold my-8'>
-          Добавление нового проката
-        </h1>
-        <Card>
-          <Card.Body>
-            <div className='grid grid-cols-3 gap-6'>
-              <div className='col-span-3 sm:col-span-2'>
-                <label htmlFor='company_website' className='block text-sm font-medium text-gray-700'>
-                        Website
-                </label>
-                <div className='mt-1 flex rounded-md shadow-sm'>
-                  <span
-                          className='inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm'>
-                          http://
-                  </span>
-                  <input
-                          type='text'
-                          name='company_website'
-                          id='company_website'
-                          className='focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300'
-                          placeholder='www.example.com'
-                  />
-                </div>
-              </div>
-            </div>
-
-            <div>
-              <label htmlFor='about' className='block text-sm font-medium text-gray-700'>
-                      About
-              </label>
-              <div className='mt-1'>
-                <textarea
-                        id='about'
-                        name='about'
-                        rows={3}
-                        className='shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md'
-                        placeholder='you@example.com'
-                        defaultValue={''}
-                />
-              </div>
-              <p className='mt-2 text-sm text-gray-500'>
-                      Brief description for your profile. URLs are hyperlinked.
-              </p>
-            </div>
-
-            <div>
-              <div className='block text-sm font-medium text-gray-700'>Photo</div>
-              <div className='mt-1 flex items-center'>
-                <span className='inline-block h-12 w-12 rounded-full overflow-hidden bg-gray-100'>
-                  <svg className='h-full w-full text-gray-300' fill='currentColor' viewBox='0 0 24 24'>
-                    <path
-                            d='M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z' />
-                  </svg>
-                </span>
-                <button
-                        type='button'
-                        className='ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
-                >
-                        Change
-                </button>
-              </div>
-            </div>
-
-            <div>
-              <div className='block text-sm font-medium text-gray-700'>Cover photo</div>
-              <div
-                      className='mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md'>
-                <div className='space-y-1 text-center'>
-                  <svg
-                          className='mx-auto h-12 w-12 text-gray-400'
-                          stroke='currentColor'
-                          fill='none'
-                          viewBox='0 0 48 48'
-                          aria-hidden='true'
-                  >
-                    <path
-                            d='M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02'
-                            strokeWidth={2}
-                            strokeLinecap='round'
-                            strokeLinejoin='round'
+    <>
+      <div className='flex flex-col items-center w-full h-full bg-gray-100 min-h-screen'>
+        <Container>
+          <h1 className='text-4xl text-gray-800 font-bold my-8'>
+            Добавление нового проката
+          </h1>
+          <Card>
+            <Card.Body>
+              <div className='bg-white'>
+                <div className='grid grid-cols-6'>
+                  <div className='col-span-7'>
+                    <Input
+                      placeholder='Велобайк'
+                      label='Название проекта'
+                      validation={true}
                     />
-                  </svg>
-                  <div className='flex text-sm text-gray-600'>
-                    <label
-                            htmlFor='file-upload'
-                            className='relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500'
-                    >
-                      <span>Upload a file</span>
-                      <input id='file-upload' name='file-upload' type='file' className='sr-only' />
-                    </label>
-                    <p className='pl-1'>or drag and drop</p>
                   </div>
-                  <p className='text-xs text-gray-500'>PNG, JPG, GIF up to 10MB</p>
+                  <div className='col-span-8'>
+                    <Input
+                      label='Адрес проката'
+                      placeholder='г. Новый Уренгой, Ленинградский проспект, 7'
+                      validation={true}
+                    />
+                  </div>
+                  <div className='col-span-6'>
+                    <Input
+                      placeholder='+7(999)999999'
+                      label='Номер телефона'
+                      type='tel'
+                      validation={true}
+                    />
+                  </div>
+
+                  <div className='col-span-12'>
+                    <div className='flex space-x-8 justify-between'>
+                      <div className='w-full'>
+                        <label htmlFor='schedule' className='text-sm text-gray-800 font-medium'>График работы</label>
+                        <select id='schedule' name='country' autoComplete='country'
+                                className='mt-1 block w-full mt-2 pt-2 pb-3 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'>
+                          <option>пн-пт</option>
+                          <option>пн-вс</option>
+                        </select>
+                      </div>
+
+                      <div className='w-full'>
+                        <Input
+                          label='Время начала работы'
+                          type='time'/>
+                      </div>
+
+                      <div className='w-full'>
+                        <Input
+                          label='Время окончания работы'
+                          type='time'/>
+                      </div>
+                    </div>
+                  </div>
+
+
                 </div>
               </div>
-            </div>
-          </Card.Body>
-          <Card.Footer>
-            <button
-              type='submit'
-              className='inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
-            >
-              Save
-            </button>
-          </Card.Footer>
-        </Card>
-      </Container>
-    </div>
+              <div>
+                <div className='block text-sm font-medium text-gray-700'>Загрузите логотип проката</div>
+                <div
+                  className='mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md'>
+                  <div className='space-y-1 text-center'>
+                    <svg
+                      className='mx-auto h-12 w-12 text-gray-400'
+                      stroke='currentColor'
+                      fill='none'
+                      viewBox='0 0 48 48'
+                      aria-hidden='true'
+                    >
+                      <path
+                        d='M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02'
+                        strokeWidth={2}
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
+                      />
+                    </svg>
+                    <div className='flex text-sm text-gray-600'>
+                      <label
+                        htmlFor='file-upload'
+                        className='relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500'
+                      >
+                        <span>Загрузите файл</span>
+                        <input id='file-upload' name='file-upload' type='file' className='sr-only'/>
+                      </label>
+                      <p className='pl-1'>или перетащите</p>
+                    </div>
+                    <p className='text-xs text-gray-500'>PNG, JPG, GIF до 10MB</p>
+                  </div>
+                </div>
+              </div>
+            </Card.Body>
+            <Card.Footer>
+              <Button
+                size='md'
+                color='primary'
+              >
+                Save
+              </Button>
+            </Card.Footer>
+          </Card>
+        </Container>
+      </div>
+    </>
   )
 }
 
