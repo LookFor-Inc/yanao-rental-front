9f54acbbefdc15ed67212b216d91f768cf94c8bd
oleg <burtaev.oleg@yandex.ru>
2021-05-22 20:38:11 +0300

    Add: profile page

---
+++ src/pages/Profile/ProfilePage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Container from '@/components/Container'
+import ProfileContent from '@/pages/Profile/components/ProfileContent'
+
+function ProfilePage() {
+  return (
+    <Container>
+      <ProfileContent/>
+    </Container>
+  )
+}
+
+export default ProfilePage
