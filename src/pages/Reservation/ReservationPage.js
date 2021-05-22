96cd54fe8ad7a3aa6512f1fdd0440ce5a0fa17d4
Oleg Chushkin <chushckin.ol@yandex.ru>
2021-05-22 14:27:11 +0300

    Add: reservation booking

--- src/pages/Reservation/ReservationPage.js
+++ src/pages/Reservation/ReservationPage.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Container from '@/components/Container'
 import ReservationChoiceList from '@/pages/Reservation/components/ReservationChoiceList'
+import ChoicePayment from '@/pages/Reservation/components/СhoicePaymentCard'
 
 function ReservationPage() {
   return (
     <Container>
       <ReservationChoiceList />
+      <ChoicePayment />
     </Container>
   )
 }
---
+++ src/pages/Reservation/components/ReservationBooking.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Button from '@/components/Button'
+import Card from '@/components/Card/Card'
+import Input from '@/components/Input'
+
+function ReservationBooking() {
+  return (
+    <Card className='flex flex-col justify-between p-4'>
+      <div className='flex flex-col space-y-4'>
+        <div className='flex justify-between text-md text-gray-800 font-semibold'>
+          <span>К оплате</span>
+          <span>8500 ₽</span>
+        </div>
+        <div className='flex space-x-2'>
+          <Input
+          placeholder='Промокод'
+          validation={false}
+          />
+          <Button color='primary'>Применить</Button>
+        </div>
+      </div>
+      <Button color='primary'>Перейти к оформлению</Button>
+    </Card>
+  )
+}
+
+export default ReservationBooking
