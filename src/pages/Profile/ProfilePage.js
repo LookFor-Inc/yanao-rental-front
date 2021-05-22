e9c8453d916665dc8be5ac4f83252d539837522d
Valery Akimov <valery.e.akimov@gmail.com>
2021-05-22 21:57:56 +0300

    Add: profile header

--- src/pages/Profile/ProfilePage.js
+++ src/pages/Profile/ProfilePage.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Container from '@/components/Container'
 import ProfileContent from '@/pages/Profile/components/ProfileContent'
+import ProfileHeader from '@/pages/Profile/components/ProfileHeader'
 
 function ProfilePage() {
   return (
     <Container>
-      <ProfileContent/>
+      <ProfileHeader />
+      <ProfileContent />
     </Container>
   )
 }
