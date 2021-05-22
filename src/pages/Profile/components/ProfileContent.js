d5b1423f19977ee0ecbbc56416f45dac5d5d9e71
Oleg Chushkin <chushckin.ol@yandex.ru>
2021-05-23 02:53:08 +0300

    Upd: reservation tab

--- src/pages/Profile/components/ProfileContent.js
+++ src/pages/Profile/components/ProfileContent.js
@@ -101,7 +101,7 @@ function ProfileContent() {
                 <tr>
                   <th scope='col'
                       className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>
-                  Место
+                  Прокат
                   </th>
                   <th scope='col'
                       className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>
